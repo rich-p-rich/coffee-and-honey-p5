c52c60f8721e7236c18e8f2efaf700bb65a20414
Richard Ricot <richricot@gmail.com>
2024-09-27 19:39:27 +0000

    Add Product, ProdVariant & Service models to products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name 
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    base_price = models.DecimalField(max_digits=6, decimal_places=2)  # Base price at the smallest product/weight combination
+    strength_rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True) # for the coffee
+    extra_services = models.ManyToManyField('Service', blank=True)  # Allows multiple services: bean grinding and gift wrapping
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=6, decimal_places=2)  # Weight of the product
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # Price for this weight variant
+
+    def __str__(self):
+        return f"{self.product.name} - {self.weight}g" # g = grams
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
+    surcharge = models.DecimalField(max_digits=6, decimal_places=2)  # Additional cost for the service
+
+    def __str__(self):
+        return self.name
