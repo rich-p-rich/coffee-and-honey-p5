04d5535d5962ebd1f052bad1c60082fb9834c0c8
Richard Ricot <richricot@gmail.com>
2024-09-28 14:04:07 +0000

    Update prod detail page & add images; optimise lg navbar

--- products/views.py
+++ products/views.py
@@ -17,11 +17,10 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """ A view to display a single product """
-
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': products,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
