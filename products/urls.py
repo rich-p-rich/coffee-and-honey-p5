b3289c71d58b6311d944a098c824164ef7ccb2d6
Richard Ricot <richricot@gmail.com>
2024-09-28 15:31:27 +0000

    Adapt product views & navbar to filter by category/name

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('', views.all_products, name='all_products'),  # Homepage with filtering functionality
+    path('<int:product_id>/', views.product_detail, name='product_detail'),  # Detail page for each product
 ]
+
--- products/views.py
+++ products/views.py
@@ -4,16 +4,30 @@ from .models import Category, Product, ProductVariant, Service
 
 # Create your views here.
 
-def all_products(request):
-    """ A view to all products, plus sort and search queries """
+# All products view with filtering 
 
-    products = Product.objects.all()
+def all_products(request):
+    """ A view to display all products, plus sort and search queries """
+    
+    category = request.GET.get('category', None)  # Get category from URL query parameters
+    product_name = request.GET.get('name', None)  # Get product name from URL query parameters
+
+    if category:
+        products = Product.objects.filter(category__name__icontains=category)  # Filter by category name
+    elif product_name:
+        products = Product.objects.filter(name__icontains=product_name)  # Filter by product name
+    else:
+        products = Product.objects.all()  # Show all products if no filter is selected
 
     context = {
         'products': products,
+        'selected_category': category,
+        'selected_product_name': product_name,
     }
 
     return render(request, 'products/products.html', context)
+
+# Product Detail Page
 
 def product_detail(request, product_id):
     """ A view to display a single product """
