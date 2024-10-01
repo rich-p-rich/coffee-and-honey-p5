4b3f50277475af871376c26bd4b50ba6d61fe66f
Richard Ricot <richricot@gmail.com>
2024-10-01 20:46:23 +0000

    chore(admin): reorganize product logic in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
-from .models import Category, Product, ProductVariant, Service 
+from .models import Category, Product, ProductVariant, Service
 
-# Register your models here.
+class ProductVariantInline(admin.TabularInline):  
+    model = ProductVariant
+    extra = 1  
+
+# Custom admin for Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'rating')  
+    list_filter = ('category',)  
+    search_fields = ('name', 'description')  
+    inlines = [ProductVariantInline]  
+
+# Custom admin for ProductVariant
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ('product', 'weight', 'price') 
+    list_filter = ('product',)  
+    search_fields = ('product__name',)  
+
+# Custom admin for Service
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surcharge')  
+    search_fields = ('name',)  
+
 
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductVariant)
-admin.site.register(Service)
