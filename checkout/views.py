d122229407ae58f6d082fbe0f4d4a9142921eb43
Richard Ricot <richricot@gmail.com>
2024-10-05 20:52:14 +0000

    feat: add checkout_success page

--- checkout/views.py
+++ checkout/views.py
@@ -137,6 +137,7 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
+    """
     if request.user.is_authenticated:
         # profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
@@ -157,6 +158,7 @@ def checkout_success(request, order_number):
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
+                """
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
