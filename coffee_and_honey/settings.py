e66d114b034faf9c2bfd4c76b82ec9efe4ce60e6
Richard Ricot <richricot@gmail.com>
2024-10-07 19:38:40 +0000

    fix: correct block name typo in base.html for checkout CSS

--- coffee_and_honey/settings.py
+++ coffee_and_honey/settings.py
@@ -164,9 +164,13 @@ CURRENCY_SYMBOL = '€'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# Static and media files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
