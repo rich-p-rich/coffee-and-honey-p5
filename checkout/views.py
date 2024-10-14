0a632bfc61c78ba2d1be01b81e89f104c0aa9ebe
Richard Ricot <richricot@gmail.com>
2024-10-14 20:32:50 +0000

    feat: add order history, fix profile URL in navbar

--- checkout/views.py
+++ checkout/views.py
@@ -9,8 +9,8 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 
 from products.models import Product
-#from profiles.forms import UserProfileForm
-#from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
@@ -178,9 +178,8 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    """
     if request.user.is_authenticated:
-        # profile = UserProfile.objects.get(user=request.user)
+        profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
         order.user_profile = profile
         order.save()
@@ -199,7 +198,6 @@ def checkout_success(request, order_number):
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
-                """
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
