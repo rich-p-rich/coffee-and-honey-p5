4c254bbb0f642fb0c3736d9658922ce33fb9e602
Richard Ricot <richricot@gmail.com>
2024-10-18 20:41:09 +0000

    feat: update profile urls for address CRUD

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('saved_addresses/', views.saved_addresses, name='saved_addresses'),
+    path('edit_address/<int:address_id>/', views.edit_address, name='edit_address'),
+    path('delete_address/<int:address_id>/', views.delete_address, name='delete_address'),
 ]
