f6c386736948e7338c4fa979e30d1d75119d6349
Richard Ricot <richricot@gmail.com>
2024-10-28 09:58:24 +0000

    chore: set debug to false, prep for Heroku

--- coffee_and_honey/settings.py
+++ coffee_and_honey/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t*3%xp630g9umvf+-@wywv)zb&z(ep=khf$y$!!f&o2*+u_0#1'
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Set to False for production
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-richprich-coffeeandhone-vdd3sltlmyu.ws.codeinstitute-ide.net', 'localhost',
                 '.herokuapp.com']
