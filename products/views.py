c512d2f2ab58e989d2c2b682162dbf38b2215387
Richard Ricot <richricot@gmail.com>
2024-11-05 23:25:56 +0000

    chore: update products views for PEP8 compliance

--- products/views.py
+++ products/views.py
@@ -3,18 +3,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product, ProductVariant, Service
 from djmoney.models.fields import MoneyField
 
-# All products plus filtering by category & name  
+# All products plus filtering by category & name
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    
-    category = request.GET.get('category', None) 
+
+    category = request.GET.get('category', None)
     product_name = request.GET.get('name', None)
 
-    if category:
-        products = Product.objects.filter(category__name__icontains=category)  # Filter by category
-    elif product_name:
-        products = Product.objects.filter(name__icontains=product_name)  # Filter by product
+    if category:  # Filter by category
+        products = Product.objects.filter(category__name__icontains=category)
+    elif product_name:  # Filter by product
+        products = Product.objects.filter(name__icontains=product_name)
     else:
         products = Product.objects.all()
 
