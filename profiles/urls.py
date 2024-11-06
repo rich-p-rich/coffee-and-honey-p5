29d2796c01427f2eb63ecc7cdeda84b62392e8e7
Richard Ricot <richricot@gmail.com>
2024-11-06 08:52:58 +0000

    chore: format profiles.urls for PEP8 compliance

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,11 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path(
+        'order_history/<order_number>/',
+        views.order_history, name='order_history'),
     path('saved_addresses/', views.saved_addresses, name='saved_addresses'),
     path('add_address/', views.add_address, name='add_address'),
-    path('edit_address/<int:address_id>/', views.edit_address, name='edit_address'),
-    path('delete_address/<int:address_id>/', views.delete_address, name='delete_address'),
-    path('set_default_address/<int:address_id>/', views.set_default_address, name='set_default_address'),
-    path('set_billing_as_default/', views.set_billing_as_default, name='set_billing_as_default'),
+    path('edit_address/<int:address_id>/',
+         views.edit_address, name='edit_address'),
+    path('delete_address/<int:address_id>/',
+         views.delete_address, name='delete_address'),
+    path('set_default_address/<int:address_id>/',
+         views.set_default_address, name='set_default_address'),
+    path('set_billing_as_default/',
+         views.set_billing_as_default, name='set_billing_as_default'),
 ]
