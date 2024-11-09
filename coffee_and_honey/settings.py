53391824fe09757a657015ac49128f0311c164ba
Richard Ricot <richricot@gmail.com>
2024-11-09 09:11:47 +0000

    chore: set dbug to True

--- coffee_and_honey/settings.py
+++ coffee_and_honey/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # Debug is True for Gitpod and dev
 # Debug is False for production and submission
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://8000-richprich-coffeeandhone-vdd3sltlmyu.ws.codeinstitute-ide.net',  # noqa
                 '8000-richprich-coffeeandhone-vdd3sltlmyu.ws.codeinstitute-ide.net', 'localhost',  # noqa
